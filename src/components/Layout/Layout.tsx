ff377cc35221f3594fc70a14f63bd85ee7f4ca72
Luca Gandini <luca.gandini88@gmail.com>
2020-12-30 17:00:19 +0100

    Styled component rendering

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,17 +2,16 @@ import styled from "styled-components";
 
 interface LayoutProps {}
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const MainContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    width: 100%;
-    padding: 0 1.5rem;
-  `;
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 100%;
+  padding: 0 1.5rem;
+`;
 
-  return <MainContainer>{children}</MainContainer>;
-};
+const Layout: React.FC<LayoutProps> = ({ children }) => <MainContainer>{children}</MainContainer>;
+;
 
 export default Layout;
--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,33 +1,41 @@
+import { useCallback, useState } from "react";
 import styled from "styled-components";
 
 interface MessageProps {
-    text?: string;
+  text?: string;
 }
 
-const Message: React.FC<MessageProps> = ({ text }) => {
-    const TextField = styled.div`
-        width: 100%;
-        padding: 20px;
-        border: 1px solid #b9b9b9;
-        border-radius: 15px;
-        min-height: 200px;
-        max-height: 400px;
-        overflow-y: auto;
-        &:focus {
-            border-radius: 15px;
-            outline: none;
-        }
-    `;
-    const Placeholder = styled.span`
-        font-style: italic;
-        color: #444;
-    `;
+const Textarea = styled.textarea`
+  width: 100%;
+  padding: 20px;
+  border: 1px solid #b9b9b9;
+  border-radius: 15px;
+  min-height: 300px;
+  overflow-y: auto;
+  resize: none;
+  &:focus {
+    border-radius: 15px;
+    outline: none;
+  }
+`;
 
-    return (
-        <TextField contentEditable>
-            {text ?? <Placeholder>Code to analyze ...</Placeholder>}
-        </TextField>
-    );
+const Message: React.FC<MessageProps> = ({}) => {
+  const [text, setText] = useState<string>();
+
+  const messageChangeHandler = useCallback(
+    (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setText(event.target.value);
+    },
+    []
+  );
+
+  return (
+    <Textarea
+      placeholder="Code to analize ..."
+      onChange={messageChangeHandler}
+      value={text}
+    />
+  );
 };
 
 export default Message;
